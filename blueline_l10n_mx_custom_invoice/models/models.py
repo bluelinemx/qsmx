f1629b2a21d3cc2551e4e2dd844ef8e74c093b4f
Yusnel Rojas Garcia <yrojass@gmail.com>
2018-03-06 10:16:40 -0500

    fix view when invoice have payments

--- blueline_l10n_mx_custom_invoice/models/models.py
+++ blueline_l10n_mx_custom_invoice/models/models.py
@@ -4,3 +4,11 @@ from odoo import models, fields, api
 from datetime import datetime
 
 from odoo.tools import DEFAULT_SERVER_TIME_FORMAT
+
+
+class Invoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.model
+    def get_l10n_mx_edi_usages(self):
+        return dict(self._fields['l10n_mx_edi_usage'].selection)

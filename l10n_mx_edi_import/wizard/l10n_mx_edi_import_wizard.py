685b93d22430d9eeb994b3dae4041201e8a9e839
Yusnel Rojas Garcia <yrojass@gmail.com>
2018-03-26 11:20:08 -0400

    minor improvements

--- l10n_mx_edi_import/wizard/l10n_mx_edi_import_wizard.py
+++ l10n_mx_edi_import/wizard/l10n_mx_edi_import_wizard.py
@@ -252,7 +252,7 @@ class EdiImport(models.TransientModel):
                 'type': 'out_invoice',
                 'state': 'draft',
                 'reference': False,
-                'move_name': self.name,
+                'move_name': "F-/{name}".format(name=self.name),
                 'date_invoice': self.date_invoice,
                 'partner_shipping_id': self.partner_shipping_id.id,
                 'l10n_mx_edi_usage': self.l10n_mx_edi_usage,

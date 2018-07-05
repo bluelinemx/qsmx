6bab3fd9a3f8fd8f1a35428fb52d5eb3835f6d1a
Yusnel Rojas Garcia <yrojass@gmail.com>
2018-07-05 14:51:01 -0400

    add qsmx module for modifying backend

---
+++ qsmx_web/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "QSMX Web Modifications",
+
+    'description': """
+        QSMX Web Modifications
+    """,
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'qweb': ['static/src/xml/*.xml'],
+}
